c1b7b89def8ebb64da625704af80db3567d9c735
needleworm <bhban@kakao.com>
2022-03-04 17:39:05 +0900

    .

--- src/App.js
+++ src/App.js
@@ -252,13 +252,13 @@ class App extends Component {
             <div className="panel-main__inner panel-inverted">
               <div className="panel-main__content">
                 {profilePicButton}
-                <h1 className="panel-cover__title panel-title">Byunghyun Ban</h1>
+                <h1 className="panel-cover__title panel-title">Hyoseok Lee</h1>
                 <nav className="cover-navigation navigation--social">
                       {blog}
                 </nav>
                 <hr className="panel-cover__divider"/>
-                <p className="panel-cover__description">Author, Entrepreneur, Researcher and Developer</p>
-                <p className="panel-cover__description">Working at <a href="https://sangsang.farm">Imagination Garden Inc.</a> as CTO.</p>
+                <p className="panel-cover__description">Author, Designer and Developer</p>
+                <p className="panel-cover__description">Working at <a href="https://www.skia.kr/">SKIA</a> as XR Specialist.</p>
                 <hr className="panel-cover__divider panel-cover__divider--secondary"/>
                 <div className="navigation-wrapper">
                   <br/>
